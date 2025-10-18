0ff936819d67c0ad169e0cd753de7a23d402071e
Sashait-wq <shevchenoolexandr19@gmail.com>
2025-10-18 23:33:48 +0100

    feat: initialize ApiService structure and base methods

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,9 +1,34 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private _baseUrl: string = 'https://dummyjson.com/';
 
-  constructor() { }
+  constructor(private _http: HttpClient) {}
+
+  public get<T>(endpoint: string, params?: HttpParams) {}
+
+  public post<T>(endpoint: string, body: any) {}
+
+  public delete<T>(endpoint: string) {}
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-type': 'application/json',
+    });
+  }
+
+  private handleError(error: HttpErrorResponse): never {
+    console.error('API Error:', error);
+    throw error;
+  }
 }
